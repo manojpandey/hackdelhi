fec39b30d3c261e1cf983156a2890a034d6a37c7
Manoj Pandey <manojpandey1996@gmail.com>
2015-07-26 03:06:04 +0530

    blah

--- run.py
+++ run.py
@@ -1,8 +1,30 @@
-#!flask/bin/python
+import os
+from flask import Flask, render_template, session, redirect, url_for
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
 
-from app import app
-from flask.ext.script import Manager
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
-manager = Manager(app)
-manager.run()
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+def main():
+	app.run()
+
+if __name__ == '__main__':
+	main()
